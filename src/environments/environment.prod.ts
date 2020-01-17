5c50bf31f71bcd65da2a9075a207b2131074620b
aherbozo <aherbozo@sintad.net>
2020-01-17 18:04:58 -0500

    cambioapi

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: "http://mybalancercf-1743047021.us-east-1.elb.amazonaws.com"
+  //apiUrl: "http://mybalancercf-1743047021.us-east-1.elb.amazonaws.com"
+    apiUrl:"https://5abmd5211c.execute-api.us-east-1.amazonaws.com/prod"
 };
